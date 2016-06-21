1840beb0c1a93a11aa4a9cdd9999d2f76a26d74b
Eli Zoller <zoller.eli@gmail.com>
2016-06-21 14:35:56 -0400

    attempting fix for language with text type

--- lib/mods_display/fields/language.rb
+++ lib/mods_display/fields/language.rb
@@ -3,7 +3,7 @@ class ModsDisplay::Language < ModsDisplay::Field
     return_fields = @values.map do |value|
       if value.respond_to?(:languageTerm)
         value.languageTerm.map do |term|
-          if term.attributes["type"].respond_to?(:value) and term.attributes["type"].value == "code"
+          if term.attributes["type"].respond_to?(:value) and (term.attributes["type"].value == "code" || term.attributes["type"].value == "text")
             ModsDisplay::Values.new(:label => displayLabel(value) || displayLabel(term) || I18n.t('mods_display.language'), :values => [displayForm(value) || language_codes[term.text]].flatten)
           end
         end.flatten.compact
